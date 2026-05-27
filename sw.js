5d6542708d3dd2e2c7a7fd9180cf317e0a9c8bc7
AxelPalafox <axel.palafox@hotmail.es>
2026-05-27 16:59:30 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,36 @@
-const CACHE_NAME = 'vitalcheck-v2';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./index.html', './manifest.json']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
+const CACHE_NAME = 'vitalcheck-v3';
+const urlsToCache = ['./index.html', './manifest.json'];
+
+// Instalar y forzar la nueva versión inmediatamente
+self.addEventListener('install', event => {
+  self.skipWaiting(); 
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+// Borrar las versiones antiguas (la v1 y v2 que se quedaron trabadas)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Estrategia "Network First" (Primero internet, luego caché)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Si hay internet, guarda la nueva versión en caché y muéstrala
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request)) // Si no hay internet, usa el caché
+  );
+});
